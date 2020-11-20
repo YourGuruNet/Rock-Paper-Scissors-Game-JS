71c138ce0917e76461171a6cbe997d8ad261ce35
YourGuruNet <arvels123@inbox.lv>
2020-11-20 12:46:01 +0200

    Finished games visualization

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_CHOSE = ROCK;
-
-const getPlayerChoice = function () {
+const DRAW = "DRAW";
+const PLAYER_WINS = "PLAYER WINS";
+const COMPUTER_WINS = "COMPUTER WINS";
+let gameIsRunning = false;
+const getPlayerChoice = () => {
   const selection = prompt(
     `${ROCK}, ${PAPER} or ${SCISSORS}?`,
     "Your answer"
@@ -17,12 +20,77 @@ const getPlayerChoice = function () {
   return selection;
 };
 
-startGameBtn.addEventListener("click", function () {
+const getComputerChoice = () => {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.64) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const showPlayersChose = function (playerChoice) {
+  if (playerChoice === ROCK) {
+    document.getElementById("player").classList.add("rock");
+  } else if (playerChoice === PAPER) {
+    document.getElementById("player").classList.add("paper");
+  } else if (playerChoice === SCISSORS) {
+    document.getElementById("player").classList.add("scissors");
+  }
+};
+
+const showComputerChose = function (computerChoice) {
+  if (computerChoice === ROCK) {
+    document.getElementById("computer").classList.add("rock");
+  } else if (computerChoice === PAPER) {
+    document.getElementById("computer").classList.add("paper");
+  } else if (computerChoice === SCISSORS) {
+    document.getElementById("computer").classList.add("scissors");
+  }
+};
+
+const getWinner = (computerChoice, playerChoice) =>
+  computerChoice === playerChoice
+    ? DRAW
+    : (computerChoice === ROCK && playerChoice === PAPER) ||
+      (computerChoice === PAPER && playerChoice === SCISSORS) ||
+      (computerChoice === SCISSORS && playerChoice === ROCK)
+    ? PLAYER_WINS
+    : COMPUTER_WINS;
+
+/*  
+  if (computerChoice === playerChoice) {
+    return DRAW;
+  } else if (
+    (computerChoice === ROCK && playerChoice === PAPER) ||
+    (computerChoice === PAPER && playerChoice === SCISSORS) ||
+    (computerChoice === SCISSORS && playerChoice === ROCK)
+  ) {
+    return PLAYER_WINS;
+  } else {
+    return COMPUTER_WINS;
+  }*/
+
+startGameBtn.addEventListener("click", () => {
   if (gameIsRunning) {
     return;
   }
   gameIsRunning = true;
-  console.log("Game is starting.");
-  const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  showPlayersChose(playerChoice);
+  showComputerChose(computerChoice);
+  let message;
+  if (winner === DRAW) {
+    message = `You have a draw! You picked ${playerChoice}, and computer also picked ${computerChoice}!`;
+  } else if (winner === PLAYER_WINS) {
+    message = `you win this game! You picked ${playerChoice}, but computer picked ${computerChoice}!`;
+  } else {
+    message = `Computer win this game! You picked ${playerChoice}, but computer picked ${computerChoice}!`;
+  }
+  document.getElementById("winner").innerHTML = message;
+  gameIsRunning = false;
 });
